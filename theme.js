3c423c28cf104a1d4d7b2f2bf8d66ed73f1cff23
jeffsturm4nn <jefferson360@gmail.com>
2021-08-04 02:53:35 -0300

    Nova imagem: iPhone Black Kitty + code changes

--- theme.js
+++ theme.js
@@ -1,1 +1,19 @@
 
+
+function isDarkThemeEnabled(){
+    return document.body.classList.contains('dark-theme');
+}
+
+function switchTheme(){
+    document.body.classList.toggle('dark-theme');
+    
+    if(isDarkThemeEnabled()){
+        document.getElementById("switch-theme-bt").innerHTML = "Light Mode ☀";
+        document.getElementById("instagram-phone-img").src = "./img/instagram-celular-dark.png"; 
+        document.getElementById("instagram-logo-img").style.filter = "invert(100%)";
+    } else {
+        document.getElementById("switch-theme-bt").innerHTML = "Night Mode 🌙";
+        document.getElementById("instagram-phone-img").src = "./img/instagram-celular.png"; 
+        document.getElementById("instagram-logo-img").style.filter = "invert(0%)";
+    }
+}
