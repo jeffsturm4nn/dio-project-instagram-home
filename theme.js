5ba608066846182aca995b2f7399f85d31f739c0
jeffsturm4nn <jefferson360@gmail.com>
2021-08-05 03:03:41 -0300

    Versão final da pagina.

--- theme.js
+++ theme.js
@@ -8,12 +8,14 @@ function switchTheme(){
     document.body.classList.toggle('dark-theme');
     
     if(isDarkThemeEnabled()){
-        document.getElementById("switch-theme-bt").innerHTML = "Light Mode ☀";
-        document.getElementById("instagram-phone-img").src = "./img/instagram-celular-dark.png"; 
+        document.getElementById("switch-theme-bt").innerHTML = "Light Mode 🌞";
+        document.getElementById("instagram-phone-img").style.display = "none";
+        document.getElementById("instagram-phone-black-img").style.display = "flex";
         document.getElementById("instagram-logo-img").style.filter = "invert(100%)";
     } else {
         document.getElementById("switch-theme-bt").innerHTML = "Night Mode 🌙";
-        document.getElementById("instagram-phone-img").src = "./img/instagram-celular.png"; 
+        document.getElementById("instagram-phone-img").style.display = "flex";
+        document.getElementById("instagram-phone-black-img").style.display = "none";
         document.getElementById("instagram-logo-img").style.filter = "invert(0%)";
     }
 }
